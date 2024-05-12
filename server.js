8c792dbe6f45757542d27d337e176d49909df7a0
guilhermehuther <guilhermehuther@gmail.com>
2024-05-11 22:57:43 -0300

    create: docs

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require("express");
 var body_parser = require("body-parser");
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Users API",
+            description: "Users API Information",
+            contact: {
+                name: "Guilherme Huther",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/api",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.js"],
+};
+const swaggerDocs = swaggerjsdoc(swaggerOptions);
 
 const routes = require("./src/routes/routes.js");
 
 var app = express();
-
 app.use(body_parser.json({ type: "application/json", limit: "25mb" }));
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/api", routes);
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}/api`);
+    console.log(`http://localhost:${PORT}/api`);
 });
