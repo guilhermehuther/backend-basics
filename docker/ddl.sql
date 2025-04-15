241892a04f1ec4ff426135e30fb4ff6d6b6e69ff
guilhermehuther <guilherme.huther@a3data.com.br>
2025-04-15 19:41:39 -0300

    feat: populate database

--- docker/ddl.sql
+++ docker/ddl.sql
@@ -7,3 +7,12 @@ CREATE TABLE users (
     email_users         VARCHAR(255) NOT NULL,
     created_at_users    TIMESTAMP DEFAULT NOW()
 );
+
+INSERT INTO public.users (id_users, password_users, name_users, email_users, created_at_users)
+SELECT
+  gen_random_uuid(),
+  md5(random()::text),
+  'User' || i,
+  'user' || i || '@example.com',
+  now() - (random() * interval '365 days')
+FROM generate_series(1, 100) AS s(i);
